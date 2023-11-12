6e64162aa835f3fbacde8e8a615aec80ec21e303
best-coder-001 <138012612+best-coder-001@users.noreply.github.com>
2023-11-12 23:08:17 +0600

    req.txt

--- bookmarket/book_app/models.py
+++ bookmarket/book_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Books(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    rating = models.FloatField(max_length=5)
+    count_pages = models.IntegerField()
+    author_id = models.ManyToManyField('Authors')
+    slug = models.SlugField(unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+class Genres(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Authors(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, db_index=True)
+
+
+    def __str__(self):
+        return self.name
